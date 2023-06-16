a276978acd5d41243f01d4ce3b117bc15bcdefe2
Sheldon Lee <liwentao0523@hotmail.com>
2023-06-16 14:08:45 +0800

    test

--- test/test.js
+++ test/test.js
@@ -7,11 +7,11 @@ chai.use(chaiString);
 
 describe('User Registration', () => {
   const agent = request.agent(app);
-  it('get WAnode', () => {
-    agent.get('/WAnode?query=1&key=n').expect(200).end((err, res) => {
-      chai.expect(res.body).to.be.an('array');
-    });
-  });
+  // it('get WAnode', () => {
+  //   agent.get('/WAnode?query=1&key=n').expect(200).end((err, res) => {
+  //     chai.expect(res.body).to.be.an('array');
+  //   });
+  // });
 
   it('get key', async () => {
     const res = await agent.get('/key').expect(200);
